57ccd61fbffe8cd3b2d9c1901303a3b6a501fc68
Carol <carol5@snet.net>
2018-10-03 14:28:03 -0400

    update and add styles

--- landingPage.js
+++ landingPage.js
@@ -1,22 +1,20 @@
-let = tableLabel = ["Pros" ,"Cons"];
+//append to table in html
+
+let = tableLabel = ["Pros", "Cons"];
 let = build = {
-    "Pros":["Fresh New Home", "Quality Constructed", "Customization","Energy Efficient","No Immediate Repairs"],
-    "Cons":["Not much pricing negotiation","post move costs,ie landscaping","Time commitment","Dealing with Planning and Zoning"]
+    "Pros": ["Fresh New Home", "Quality Constructed", "Customization", "Energy Efficient", "No Immediate Repairs"],
+    "Cons": ["Not much pricing negotiation", "post move costs,ie landscaping", "Time commitment", "Dealing with Planning and Zoning","Finding the right Contractor"]
 }
-    //buildTable("#table1",tableLabel,build);
-
-/*function buildTable(){
-    let sel = document.querySelector("#table1").innerHTML()
-    console.log (build);
-
-}*/
-let columns = ["pros","cons"]
-for (let i = 0; i < build.length; i++ ){
-    $("#table1").append("<tr></tr>");
-    
-    for(let j = 0; j< columns.length; j++){
-        $("#table1").append("<td>" + build[i][columns[j]+ "</td>"])
-        console.log(build[i]);
+function tableData() {
+    for (const builds of Object.values(build.Pros)) {
+        console.log("Builds Stuff:", builds);
+        $("#table1").append(`<tr> <td>${builds}</td></tr>`)
     }
+    for (const Builds of Object.values(build.Cons)) {
+        console.log("Build More Stuff:", Builds);
+        $("#table2").append(`<tr> <td>${Builds}</td></tr>`)
+    }
+
 }
-//$("table1").append("<td>"+build.pros[0]+"</td>")
+tableData();
+
---
+++ readMe.md
@@ -0,0 +1,4 @@
+Free Code Camp project 
+Passed their specifications
+added styling and an append table to increase my knowledge 
+incorporated google fonts , bootstrap , jQuery
